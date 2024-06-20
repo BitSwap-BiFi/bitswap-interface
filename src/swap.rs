d31b425e9e7f8f07e76e55dde5bfcd57d7bd72f9
223880 <rafael.segala45@gmail.com>
2024-06-20 09:03:52 -0300

    Fix

--- src/swap.rs
+++ src/swap.rs
@@ -1,1 +1,13 @@
+use crate::core::types::Swap;
+use bitswap_core::types::PeerId;
+use bitswap_core::types::Peer;
+
+impl swap {
+    pub fn new(peer: PeerId, swap: Swap) -> Self {
+        Self {
+            peer,
+            swap,
+        }
+    }
+}
 
