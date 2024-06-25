bd23bb519373dd229c6f18eeb265c57839adf850
223880 <rafael.segala45@gmail.com>
2024-06-25 09:03:42 -0300

    Fix

--- src/swap.rs
+++ src/swap.rs
@@ -1,6 +1,8 @@
 use crate::core::types::Swap;
 use bitswap_core::types::PeerId;
 use bitswap_core::types::Peer;
+use rgbstd::prelude::H160;
+use rgbstd::interface::Rgb20;
 
 impl swap {
     pub fn new(peer: PeerId, swap: Swap) -> Self {
